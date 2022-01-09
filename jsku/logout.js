748614fb2972c56e14dd473c327ec1db5afb39db
denydemang <denyk819@gmail.com>
2022-01-09 13:18:09 +0700

    memodifikasi bagian tombol keluar dengan menambahakan google spreadsheet

--- jsku/logout.js
+++ jsku/logout.js
@@ -1,4 +1,20 @@
-$('.keluar').click(function(){
+// $('.keluar').click(function(){
+//     sessionStorage.removeItem('login');
+//     sessionStorage.removeItem('disabledTebakan');
+//     sessionStorage.removeItem('disabledTebakAngka');
+//     sessionStorage.removeItem('disabledWeton');
+//     sessionStorage.removeItem('lastname');
+//     if (!(sessionStorage.getItem('udahMain'))){
+//         sessionStorage.removeItem('firstname');
+//         sessionStorage.removeItem('lastname');
+//     }
+//     document.location.href="index.html";
+// })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzo8v58JAmoH5G9a1cdwnXwF_UTxZFpECM1K1MudB9VkFLNALJQ6-zRs26mA6mUqQcn/exec'
+const form = document.forms['permainan']
+
+    form.addEventListener('submit', e => {
+    e.preventDefault()
     sessionStorage.removeItem('login');
     sessionStorage.removeItem('disabledTebakan');
     sessionStorage.removeItem('disabledTebakAngka');
@@ -7,6 +23,10 @@ $('.keluar').click(function(){
     if (!(sessionStorage.getItem('udahMain'))){
         sessionStorage.removeItem('firstname');
         sessionStorage.removeItem('lastname');
-    }
-    document.location.href="index.html";
-})
+        }
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('success') )
+        .catch(error => Swal.fire('Feedback Gagal Dikirim !' +error.message,'','warning').then(function(){sessionStorage.clear(); location.reload();}))
+
+        document.location.href="index.html";  
+    })
